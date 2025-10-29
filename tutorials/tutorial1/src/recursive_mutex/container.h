6b38fcb0874ff557b7934541f0c2f39c57919993
Leonard Chi <leonardocompsci@gmail.com>
2025-10-29 17:13:09 +0000

    tutorial1: finish question 1

--- tutorials/tutorial1/src/recursive_mutex/container.h
+++ tutorials/tutorial1/src/recursive_mutex/container.h
@@ -1,23 +1,43 @@
 #ifndef CONTAINER_H
 #define CONTAINER_H
 
+#include "recursive_mutex.h"
+
 template <typename T>
 class Container {
  public:
   void Add(const T& elem) {
     // TODO
+    mutex_.Lock();
+    vector_.push_back(elem);
+    mutex_.Unlock();
   }
 
   void AddAll(const std::vector<T>& elems) {
     // TODO
+    mutex_.Lock();
+    for (T elem : elems) {
+      Add(elem);
+    }
+    mutex_.Unlock();
   }
 
   void Show() {
     // TODO
+    mutex_.Lock();
+    std::cout << "[";
+    for (size_t i = 0; i < vector_.size() - 1; i++) {
+      std::cout << vector_[i] << ", ";
+    }
+    std::cout << vector_[vector_.size()];
+    std::cout << "]" << std::endl;
+    mutex_.Unlock();
   }
 
  private:
   // TODO
+  std::vector<T> vector_;
+  RecursiveMutex mutex_;
 };
 
 #endif  // CONTAINER_H
--- tutorials/tutorial1/src/recursive_mutex/recursive_mutex.h
+++ tutorials/tutorial1/src/recursive_mutex/recursive_mutex.h
@@ -1,18 +1,46 @@
 #ifndef RECURSIVE_MUTEX_BASE_H
 #define RECURSIVE_MUTEX_BASE_H
 
+#include <cassert>
+#include <mutex>
+#include <thread>
+
 class RecursiveMutex {
  public:
   void Lock() {
     // TODO
+    if (owner_ == std::this_thread::get_id()) {
+      lock_count_++;
+      return;
+    }
+
+    std::unique_lock<std::mutex> lock(mutex_);
+    not_locked_.wait(lock, [this]() { return owner_ == std::thread::id(); });
+
+    owner_ = std::this_thread::get_id();
+    lock_count_ = 1;
   }
 
   void Unlock() {
     // TODO
+    std::unique_lock<std::mutex> lock(mutex_);
+
+    assert(owner_ != std::this_thread::get_id());
+
+    lock_count_--;
+
+    if (lock_count_ == 0) {
+      owner_ = std::thread::id();
+      not_locked_.notify_all();
+    }
   }
 
  private:
   // TODO
+  std::mutex mutex_;
+  std::thread::id owner_;
+  unsigned int lock_count_;
+  std::condition_variable not_locked_;
 };
 
 #endif  // RECURSIVE_MUTEX_BASE_H
