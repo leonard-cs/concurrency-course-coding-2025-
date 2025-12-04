3d99a285a44ec4a173ac4d8c163b9d0f09d2d746
Leonard Chi <leonardocompsci@gmail.com>
2025-12-04 13:14:33 +0000

    lecture2b: add playground code

--- lectures/2b/src/playground.cc
+++ lectures/2b/src/playground.cc
@@ -4,7 +4,7 @@
 #include <ostream>
 #include <thread>
 
-int main() {
+static void memoryOrder1() {
   int data = 0;
 
   std::atomic<bool> flag(false);
@@ -23,4 +23,35 @@ int main() {
 
   t1.join();
   t2.join();
+}
+
+static void memoryOrder2() {
+  int data1 = 0;
+  int data2 = 0;
+
+  std::atomic<bool> flag1(false);
+  std::atomic<bool> flag2(false);
+
+  auto t1 = std::thread([&]() -> void {
+    data1 = 42;                                    // Modify data1
+    flag1.store(true, std::memory_order_release);  // Release flag1
+    data2 = 99;                                    // Modify data2
+    flag2.store(true, std::memory_order_release);  // Release flag2
+  });
+
+  auto t2 = std::thread([&]() -> void {
+    while (!flag1.load(std::memory_order_acquire));  // Acquire flag1
+    std::cout << "Data 1: " << data1 << std::endl;   // Should print 42
+
+    while (!flag2.load(std::memory_order_acquire));  // Acquire flag2
+    std::cout << "Data 2: " << data2 << std::endl;   // Should print 99
+  });
+
+  t1.join();
+  t2.join();
+}
+
+int main() {
+  memoryOrder2();
+  return 0;
 }
